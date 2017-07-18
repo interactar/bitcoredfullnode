ce00aeebb1edc8250f6dca3477d88a7be5e8a909
jailbirt <jailbirt@gmail.com>
2017-07-17 22:35:37 -0300

    runBitcored.sh

--- runBitcored.sh
+++ runBitcored.sh
@@ -1,58 +1,14 @@
 #!/bin/bash
+if [ -d mynode ];then  
+	echo node exists !!!, Using It
+	cd mynode
+else
+	echo new node, creating one.
+	bitcore create mynode
+	# bitcore create mytestnode --testnet
+fi
+#Weird duplicate entry caused by npm...
+rm -rf /usr/local/lib/node_modules/bitcore/node_modules/bitcore-node/node_modules/bitcore-lib
+cd /root/mynode
+bitcored
 
-nodeName=""
-run () 
-{
-  #Fix multiple bitcore-lib versions installed by npm.
-  #rm -rf /usr/local/lib/node_modules/bitcore/node_modules/bitcore-node/node_modules/bitcore-lib
-  ln -s /usr/local/lib/node_modules/bitcore/node_modules/bitcore-lib /usr/local/lib/node_modules/bitcore/node_modules/bitcore-node/node_modules/bitcore-lib
-  #rm -rf /root/$nodeName/insight-api/node_modules/bitcore-lib
-  #ln -s /usr/local/lib/node_modules/bitcore/node_modules/bitcore-lib /root/$nodeName/insight-api/node_modules/bitcore-lib
-
-#  rm -rf /root/$nodeName/node_modules/bitcore-wallet-service/node_modules/bitcore-lib
-#  ln -s /usr/local/lib/node_modules/bitcore/node_modules/bitcore-lib /root/$nodeName/node_modules/bitcore-wallet-service/node_modules/bitcore-lib
-
-
-  
-  cd /root/$nodeName
-  echo "Env $nodeName path $(pwd)"
-  bitcored
-}
-case $1 in
-	"livenet" )
-		   echo "$1 <<<<"
-                    nodeName=mynode
-                    if [ -d $nodeName ];then  
-                    	echo node exists.
-                    	cd /root/$nodeName
-                    else
-                    	echo new node, creating a new one.
-                    	echo bitcore create $nodeName 
-                    	bitcore create $nodeName 
-                    fi;;
-	"testnet" )
-		   echo "$1 <<<<"
-                    nodeName=mytestnode
-                    if [ -d $nodeName ];then  
-                    	echo node exists.
-                    	cd /root/$nodeName
-                    else
-                    	echo new node, creating a new one.
-                    	echo bitcore create $nodeName --testnet
-                    	bitcore create $nodeName --testnet
-                    fi;;
-	* ) 
-		echo "Please run as $0 (livenet|testnet) Optional (wallet) ";;
-esac
-
-
-#if [ -z $2 ];then
-#   echo Got it, running only a fullnodeservice
-#else
-#   echo "Got it, running with a wallet"
-#   cd /root/$nodeName
-#   bitcore install bitcore-wallet-service
-#   bitcore install insight-api
-#fi
-
-run
